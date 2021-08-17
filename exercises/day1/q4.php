d1b0d7c7c706b1f46b3231b10f91a5c02f767280
samhit-bhogavalli <samhit.bhogavalli@razorpay.com>
2021-08-17 16:26:16 +0530

    remove waste lines

--- exercises/day1/q4.php
+++ exercises/day1/q4.php
@@ -2,15 +2,9 @@
 
 $input = "{\"players\":[{\"name\":\"Ganguly\",\"age\":45,\"address\":{\"city\":\"Hyderabad\"}},{\"name\":\"Dravid\",\"age\":45,\"address\":{\"city\":\"Hyderabad\"}},{\"name\":\"Dhoni\",\"age\":37,\"address\":{\"city\":\"Hyderabad\"}},{\"name\":\"Virat\",\"age\":35,\"address\":{\"city\":\"Hyderabad\"}},{\"name\":\"Jadeja\",\"age\":35,\"address\":{\"city\":\"Hyderabad\"}},{\"name\":\"Jadeja\",\"age\":35,\"address\":{\"city\":\"Hyderabad\"}}]}";
 
-class player {
-    var $name;
-    var $age;
-    var $address;
-}
-
 $players = json_decode($input);
 
-print_r($players->players);
+//print_r($players->players);
 
 function getAgesNamesCities(& $players) {
     $ages = [];
