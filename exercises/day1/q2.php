a51cc253de846be292d31aa932c89ec6fa499cfc
samhit-bhogavalli <samhit.bhogavalli@razorpay.com>
2021-08-17 15:34:07 +0530

    add q2, q4 of day1

--- exercises/day1/q2.php
+++ exercises/day1/q2.php
@@ -1,2 +1,13 @@
 <?php
 
+$input = "1234567890";
+
+function maskPhNumber(& $number) {
+    for($i = 2; $i < 8; $i++) {
+        $number[$i] = "*";
+    }
+}
+
+maskPhNumber($input);
+
+print $input;
